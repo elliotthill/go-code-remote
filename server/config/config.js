280da94ef8cded45cd1da067a145c5f2e38259db
Elliott Hill <elliott.hill@gmail.com>
2024-03-14 14:58:15 +0000

    Migrate to config.js file to enable ENV variables loading on production

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,4 @@
+module.exports =
 {
     "development": {
         "username": "root",
@@ -17,10 +18,10 @@
     },
     "production": {
         "use_env_variable": true,
-        "username": "",
-        "password": "",
-        "database": "",
-        "host": "127.0.0.1",
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
         "dialect": "mysql",
         "dialectOptions": {
           "charset": "utf8mb4"
